cf8ab2d9e2fc3fa04ec68384140ce8434c8ac025
rubenramos <rubenbaloncesto@gmail.com>
2014-10-06 09:31:20 +0200

    PCategoria: actualizacion boton delete

--- PCategoria/PCategoria/MainWindow.cs
+++ PCategoria/PCategoria/MainWindow.cs
@@ -13,23 +13,34 @@ public partial class MainWindow: Gtk.Window
 	{
 		Build ();
 
-		treeView.AppendColumn ("id", new CellRendererText (), "text", 0);
-		treeView.AppendColumn ("nombre", new CellRendererText (), "text", 1);
-
-		listStore = new ListStore (typeof(string), typeof(string));
-
-		treeView.Model = listStore; // en java seria treeView.setModel(listStore)
+		deleteAction.Sensitive = false; 
 
 		mySqlConnection = new MySqlConnection (
 			"Server=localhost;Database=dbprueba;User ID=root;Password=sistemas"
-			);
+		);
 		mySqlConnection.Open ();
 
+		treeView.AppendColumn ("id", new CellRendererText (), "text", 0);
+		treeView.AppendColumn ("nombre", new CellRendererText (), "text", 1);
+		listStore = new ListStore (typeof(string), typeof(string));
+		treeView.Model = listStore; // en java seria treeView.setModel(listStore)
+
+		fillListStore ();
+
+		treeView.Selection.Changed += selectionChanged;
+	}
+
+	private void selectionChanged( object sender, EventArgs e) {
+		Console.WriteLine ("selectionChanged");
+		deleteAction.Sensitive = treeView.Selection.CountSelectedRows () > 0;
+	}
+
+
+		private void fillListStore() {
 		MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
 		mySqlCommand.CommandText = "select * from categoria";
 
 		MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader ();
-
 		while (mySqlDataReader.Read()) {
 			object id = mySqlDataReader ["id"].ToString();
 			//Lo convertimos con el ToString para que no de error.
@@ -50,13 +61,50 @@ public partial class MainWindow: Gtk.Window
 
 	protected void OnAddActionActivated (object sender, EventArgs e)
 	{
+		string insertSql = string.Format (
+			"insert into categoria (nombre) values ('{0}')",
+			"Nuevo " + DateTime.Now
+		);
+		Console.WriteLine ("insertSql={0}", insertSql);
+		MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
+		mySqlCommand.CommandText = insertSql;
 
-		listStore.AppendValues ("1", "uno");
+		mySqlCommand.ExecuteNonQuery ();
 
 	}
 	
 	protected void OnRefreshActionActivated (object sender, EventArgs e)
 	{
+		listStore.Clear ();
+		fillListStore ();
+	}
+	protected void OnDeleteActionActivated (object sender, EventArgs e)
+	{
+		MessageDialog messageDialog = new MessageDialog (
+			this,
+			DialogFlags.Modal,
+			MessageType.Question,
+			ButtonsType.YesNo,
+			"¿Quieres eliminar el registro?"
+		);
+		messageDialog.Title = Title;
+		ResponseType response = (ResponseType)messageDialog.Run ();
+		messageDialog.Destroy ();
+
+		if (response != ResponseType.Yes)
+			return;
+
+		TreeIter treeIter;
+		treeView.Selection.GetSelected (out treeIter);
+		object id = listStore.GetValue (treeIter, 0);
+		string deleteSql = string.Format ("delete from categoria where id={0}", id);
+		MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
+		mySqlCommand.CommandText = deleteSql;
+
+		mySqlCommand.ExecuteNonQuery (); 
+
 
 	}
+
+	
 }
--- PCategoria/PCategoria/gtk-gui/MainWindow.cs
+++ PCategoria/PCategoria/gtk-gui/MainWindow.cs
@@ -4,6 +4,9 @@ public partial class MainWindow
 {
 	private global::Gtk.UIManager UIManager;
 	private global::Gtk.Action addAction;
+	private global::Gtk.Action refreshAction;
+	private global::Gtk.Action deleteAction;
+	private global::Gtk.Action Action;
 	private global::Gtk.VBox vbox1;
 	private global::Gtk.Toolbar toolbar1;
 	private global::Gtk.ScrolledWindow GtkScrolledWindow;
@@ -17,6 +20,12 @@ public partial class MainWindow
 		global::Gtk.ActionGroup w1 = new global::Gtk.ActionGroup ("Default");
 		this.addAction = new global::Gtk.Action ("addAction", null, null, "gtk-add");
 		w1.Add (this.addAction, null);
+		this.refreshAction = new global::Gtk.Action ("refreshAction", null, null, "gtk-refresh");
+		w1.Add (this.refreshAction, null);
+		this.deleteAction = new global::Gtk.Action ("deleteAction", null, null, "gtk-delete");
+		w1.Add (this.deleteAction, null);
+		this.Action = new global::Gtk.Action ("Action", null, null, null);
+		w1.Add (this.Action, null);
 		this.UIManager.InsertActionGroup (w1, 0);
 		this.AddAccelGroup (this.UIManager.AccelGroup);
 		this.Name = "MainWindow";
@@ -27,7 +36,7 @@ public partial class MainWindow
 		this.vbox1.Name = "vbox1";
 		this.vbox1.Spacing = 6;
 		// Container child vbox1.Gtk.Box+BoxChild
-		this.UIManager.AddUiFromString ("<ui><toolbar name='toolbar1'><toolitem name='addAction' action='addAction'/></toolbar></ui>");
+		this.UIManager.AddUiFromString ("<ui><toolbar name='toolbar1'><toolitem name='addAction' action='addAction'/><toolitem name='refreshAction' action='refreshAction'/><toolitem name='deleteAction' action='deleteAction'/><toolitem name='Action' action='Action'/></toolbar></ui>");
 		this.toolbar1 = ((global::Gtk.Toolbar)(this.UIManager.GetWidget ("/toolbar1")));
 		this.toolbar1.Name = "toolbar1";
 		this.toolbar1.ShowArrow = false;
@@ -57,5 +66,7 @@ public partial class MainWindow
 		this.Show ();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler (this.OnDeleteEvent);
 		this.addAction.Activated += new global::System.EventHandler (this.OnAddActionActivated);
+		this.refreshAction.Activated += new global::System.EventHandler (this.OnRefreshActionActivated);
+		this.deleteAction.Activated += new global::System.EventHandler (this.OnDeleteActionActivated);
 	}
 }
