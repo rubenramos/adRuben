d8a7c515a61c53eff88824da938f412b943ae79a
rubenramos <rubenbaloncesto@gmail.com>
2015-01-16 12:13:30 +0100

    Añadido JHibernateCategoria

---
+++ JHibernateCategoria/src/main/java/serpis/ad/Categoria.java
@@ -0,0 +1,30 @@
+package serpis.ad;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria {
+	private Long id;
+    private String nombre;
+    
+    @Id
+    public Long getId() {
+		return id;
+    }
+
+    private void setId(Long id) {
+		this.id = id;
+    }
+    
+    public String getNombre() {
+    	return nombre;
+    }
+    
+    public void setNombre(String nombre){
+    	this.nombre = nombre;
+    }
+
+    
+    
+}
